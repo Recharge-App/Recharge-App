682b2ffe59fa919acd40b82a475e253711274121
Mihir Sahu <2002mihir@gmail.com>
2022-05-20 15:37:29 -0500

    I forgot what changes I made. Oops

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Appearance } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// https://ionic.io/ionicons
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { Item, HeaderButton, HeaderButtons } from "react-navigation-header-buttons";
 
 // Custom components
 import { Login, Signup } from './src/Login';
@@ -17,7 +20,12 @@ const Tab = createBottomTabNavigator();
 function MainStackNavigator() {
 	return (
       <NavigationContainer>
-	  	<Stack.Navigator>
+	  	<Stack.Navigator 
+			initialRouteName="Signup" 
+			screenOptions={({ route }) => ({
+					headerShown: false,
+				})
+			}>
       	 	<Stack.Screen name="Signup" component={Signup} />
       	 	<Stack.Screen name="Login" component={Login} />
       	</Stack.Navigator>
@@ -25,13 +33,49 @@ function MainStackNavigator() {
 	);
 }
 
-function MainTabNavigator() {
+function MainTabNavigator({ navigation }: { navigation: any }) {
 	return (
     	<NavigationContainer>
-		  <Tab.Navigator initialRouteName="Profile">
-    	    <Tab.Screen name="Home" component={Home} />
-    	    <Tab.Screen name="Profile" component={Profile} />
-    	    <Tab.Screen name="Search" component={Search} />
+		  <Tab.Navigator initialRouteName="Profile"
+			  screenOptions={({ route }) => ({
+        	  	tabBarIcon: ({ focused, color, size }) => {
+        	  	  let iconName;
+			  	  color = '#EF233C';
+
+        	  	  if (route.name === 'Home') {
+        	  	    iconName = "home";
+        	  	  } else if (route.name === 'Profile') {
+        	  	    iconName = "person-circle-outline";
+        	  	  }
+        	  	  else if (route.name === 'Search') {
+        	  	    iconName = "search";
+        	  	  }
+
+        	  	  return <Ionicons name={iconName} size={size} color={color} />;
+        	  	},
+        	  	tabBarInactiveTintColor: '#EF233C',
+        	  	tabBarActiveTintColor: '#EF233C',
+				tabBarShowLabel: false,
+        	})}
+		  >
+    	  	 <Tab.Screen name="Home" component={Home} />
+    	  	 <Tab.Screen name="Search" component={Search} />
+    	  	 <Tab.Screen 
+			 	name="Profile" 
+				component={Profile} 
+				options={{ 
+					headerRight: () => (
+						//<Button title="Settings" onPress={() => alert("Hello")} />
+						<HeaderButtons>
+					        <Item
+          					title="Setting"
+          					iconName="ios-settings-outline"
+          					onPress={() => navigation.navigate('Profile')}
+        					/>
+						</HeaderButtons>
+					)
+				}}
+			/>
     	  </Tab.Navigator>
     	</NavigationContainer>
 	);
@@ -39,7 +83,8 @@ function MainTabNavigator() {
 
 function App() {
   return (
-	<MainTabNavigator/>
+	//<MainTabNavigator />
+	<MainStackNavigator/>
   );
 }
 
