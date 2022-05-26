2a8fc308db5f4ca62e8a4faa9d6b2bb1e4318e6d
Fernando <ramirez.fernando2003@gmail.com>
2022-05-26 11:25:48 -0500

    Updated Onboarding page conents

--- cougar-connect/src/Data/slides.ts
+++ cougar-connect/src/Data/slides.ts
@@ -1,27 +1,32 @@
+import people from '../../assets/images/people.png';
+import posts from '../../assets/images/posts.png';
+import feedback from '../../assets/images/feedback.png';
+import happy from '../../assets/images/happy.png';
+
 export default [
 
 {
     id: '1',
-    title: 'Getting started',
-    description: 'Share and host events',
-    image: require('../../assets/CougarCS.jpeg'),
+    title: 'Welcome to Cougar Connect',
+    description: 'All events at the University of Houston in one place, instantly searchable.',
+    image: people,
 },
 {
     id: '2',
-    title: 'hi',
-    description: 'hi again',
-    image: require('../../assets/CougarCS.jpeg'),
+    title: 'Stay connected',
+    description: 'Learn about upcoming events, or create one yourself!',
+    image: posts,
 },
 {
     id: '3',
-    title: 'there',
-    description: 'cool',
-    image: require('../../assets/CougarCS.jpeg'),
+    title: 'Leave feedback',
+    description: 'Comment under a posts or send one directly to the event creator.',
+    image: feedback,
 },
 {
     id: '4',
-    title: 'last',
-    description: 'test',
-    image: require('../../assets/CougarCS.jpeg'),
+    title: 'Getting started',
+    description: 'Create your account, optionally create an organization, and share events!',
+    image: happy,
 },
 ];
