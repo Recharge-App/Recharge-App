0a88f00641ff4d84d7454be87384666cb8866a4f
Sana Akbani <86171834+sanaakbani@users.noreply.github.com>
2022-07-24 10:35:28 -0700

    Update Fonts.tsx
    Removing Lato_700Bold font to get rid of the "Font not found" bug

--- cougar-connect/src/Components/Fonts.tsx
+++ cougar-connect/src/Components/Fonts.tsx
@@ -5,7 +5,6 @@ export const fonts = {
 	Lato_300Light_Italic: 'Lato_300Light_Italic',
 	Lato_400Regular: 'Lato_400Regular',
 	Lato_400Regular_Italic: 'Lato_400Regular_Italic',
-	Lato_700Bold: 'Lato_700Bold',
 	Lato_700Bold_Italic: 'Lato_700Bold_Italic',
 	Lato_900Black: 'Lato_900Black',
 	Lato_900Black_Italic: 'Lato_900Black_Italic',
