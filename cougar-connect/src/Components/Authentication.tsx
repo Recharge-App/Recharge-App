1cee2030613c0e63188583226d2617cee8650f6f
Mihir Sahu <2002mihir@gmail.com>
2022-07-28 17:51:06 -0500

    Added Authentication.tsx

---
+++ cougar-connect/src/Components/Authentication.tsx
@@ -0,0 +1,63 @@
+import React, {createContext, useState, useContext} from 'react';
+
+//https://levelup.gitconnected.com/react-native-authentication-flow-the-simplest-and-most-efficient-way-3aa13e80af61
+
+
+type AuthContextData = {
+    authData?: AuthData;
+    loading: boolean;
+    signIn(): Promise<void>;
+    signOut(): void;
+  };
+      
+type AuthData = {
+  email: string;
+  password: string;
+};
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export const AuthProvider: React.FC = ({children}) => {
+  const [authData, setAuthData] = useState<AuthData>();
+  
+  //The loading part will be explained in the persist step session
+  const [loading, setLoading] = useState(true);
+
+  const signIn = async () => {
+    //call the service passing credential (email and password).
+    //In a real App this data will be provided by the user from some InputText components.
+    const _authData = {
+        email: '2002mihir@gmail.com',
+        password: 'mihirsahu'
+    };
+
+    //Set the data in the context, so the App can be notified
+    //and send the user to the AuthStack
+    setAuthData(_authData);
+  };
+
+  const signOut = async () => {
+    //Remove data from context, so the App can be notified
+    //and send the user to the AuthStack
+    setAuthData(undefined);
+  };
+
+  return (
+    //This component will be used to encapsulate the whole App,
+    //so all components will have access to the Context
+    <AuthContext.Provider value={{authData, loading, signIn, signOut}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+
+export function useAuth(): AuthContextData {
+    const context = useContext(AuthContext);
+  
+    if (!context) {
+      throw new Error('useAuth must be used within an AuthProvider');
+    }
+  
+    return context;
+}
